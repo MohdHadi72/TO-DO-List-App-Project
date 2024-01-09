9d03b050cd080e21b6a30a8b7cfac14f1cb2353d
Mohd Hadi <154020781+MohdHadi72@users.noreply.github.com>
2024-01-10 02:55:16 +0530

    Change Text Color And To-do List Background Color.

--- Todo List App.js
+++ Todo List App.js
@@ -2,7 +2,61 @@
 let inputBox = document.getElementById("input-box");
 let listContainer = document.getElementById("list-container");
 
+let Todo = document.getElementById("Todoapp");
+let btn1 = document.getElementById("BTN");
 
+let buttonColor = document.getElementById("BtnColor");
+
+let textChange = document.getElementById("text");
+let inputText  = document.getElementById("input");
+
+let colorText = ["red","black"];
+
+buttonColor.addEventListener("click",()=>{
+    let randomColor = colorText[Math.floor(Math.random()* colorText.length)];
+    listContainer.style.color = randomColor; 
+    listContainer.style.fontWeight = "bold"; 
+
+    
+})
+
+
+inputText.addEventListener("input", ()=>{
+    textChange.style.color = inputText.value;
+    textChange.style.fontSize = inputText.value;
+    textChange.style.fontFamily = inputText.value;
+});
+
+
+let color = ["red","white","orange","aqua","pink"];
+
+btn1.addEventListener("click", function (){
+    let randomColor = color[Math.floor(Math.random()* color.length)];
+     Todo.style.background = randomColor; 
+
+});
+
+// yle.backgroundColor = "green";
+
+
+inputBox.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter' && inputBox.value !== ''){
+        if(inputBox.value === ""){
+            alert("You Must write Somthing!");
+        }
+        else{
+            let li = document.createElement("li");
+            li.innerHTML = inputBox.value;
+            listContainer.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";           
+            li.appendChild(span);
+        }
+    
+        inputBox.value = "";
+        saveData();
+    }
+})
 
 function addTask(){
     if(inputBox.value === ""){
@@ -11,7 +65,6 @@ function addTask(){
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        li.style.color = "red";
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
@@ -22,14 +75,17 @@ function addTask(){
     saveData();
 }
 
-listContainer.addEventListener('click', function (e) {
-    
+
+listContainer.addEventListener('click', function (e) {  
     if (e.target.tagName === "LI") {   
         e.target.classList.toggle("checked");
+      
+       
         saveData()
     }
      else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+
         saveData()
     }
 
@@ -38,7 +94,6 @@ listContainer.addEventListener('click', function (e) {
 
 function saveData() {
     localStorage.setItem("data",listContainer.innerHTML);
-    localStorage.style.color = "red";
     
 }
 
@@ -47,3 +102,4 @@ function showTask() {
 
 }
 showTask();
+
